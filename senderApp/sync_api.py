35615a1e6a5607bf502946f8141548d68ebd1cda
Terry Lau <homan03@gmail.com>
2025-11-21 11:26:42 -0500

    Update docker-compose.yml to expose port 5000 for senderapp and modify Watchtower schedule to check every minute. Update health check response in sync_api.py.

--- senderApp/sync_api.py
+++ senderApp/sync_api.py
@@ -16,7 +16,7 @@ def index():
 
 @app.route('/health')
 def health():
-    return "APIs working fine. Test 123"
+    return "APIs working fine. Test 123456"
 
 if __name__ == '__main__':
     # For development, the built-in server is fine.
